a66a59f934c37c6f318ed6dc39ec861b211c434b
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-16 15:56:31 +0000

    Automatically backed up by Learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -3,5 +3,6 @@ class PostSerializer
   def self.serialize(post)
     serialized_post = '{'
     serialized_post += '"id": ' + post.id.to_s + ', '
+    serialized_post += '"title": "' + post.title + '", '
   
 end 
